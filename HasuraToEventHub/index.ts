6f65e1885b633a9f6acd1673e0232be6e3f9f99a
soorajshankar <soorajshankar@gmail.com>
2020-12-03 20:45:35 +0530

    cleanup

--- HasuraToEventHub/index.ts
+++ HasuraToEventHub/index.ts
@@ -9,7 +9,7 @@ dotenv.config();
 const connectionString = process.env["EVENTHUB_CONNECTION_STRING"] || "";
 const eventHubName = process.env["EVENTHUB_NAME"] || "";
 // console.log({ connectionString, eventHubName })
-async function main(payload) {
+async function sendToEventHub(payload) {
 
     // Create a producer client to send messages to the event hub.
     const producer = new EventHubProducerClient(connectionString, eventHubName);
@@ -25,24 +25,19 @@ async function main(payload) {
     await producer.close();
 
     console.log("A batch of three events have been sent to the event hub");
+    return;
 }
 
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-    // console.log(JSON.stringify(req.body))
-
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: { success: true, responseMessage }
+        body: { success: true, data: req?.body || '' }
     };
     try {
         // TODO process the data here 
-        await main(req.body)
+        await sendToEventHub(req.body)
     } catch (err) {
         context.res = {
             status: 500, /* Defaults to 200 */
